9b40fb2737a34aa550b15b29194cdf78e02fe083
Naufal <nhabibie25@gmail.com>
2024-04-04 17:35:12 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import searchBtn from './search.svg'
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Aniview</h1>
+
+            <div className="search">
+                <input  
+                    placeholder="Search anime"
+                    value=''
+                    onChange={() => {}}
+                />
+
+                <img 
+                    src={searchBtn}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
