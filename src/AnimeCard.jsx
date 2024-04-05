62971de233f1cb7934321eefa44e305938526b1c
Naufal <nhabibie25@gmail.com>
2024-04-05 13:59:06 +0700

    added fetch anime data from Anime DB API

---
+++ src/AnimeCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AnimeCard = ({anime}) => {
+    return (
+        <div className="movie">
+            <div>
+                <p>{anime.myanimelist_id}</p>
+            </div>
+            <div>
+                <img src={anime.picture_url}/>
+            </div>
+            <div>
+                <h3>{anime.title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default AnimeCard;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 import './App.css'
 import searchBtn from './search.svg'
+import AnimeCard from './AnimeCard'
+
+
+ const API_URL = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10';
+ const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'caba5fe668msh1c737d945683b4ep160740jsn54377fd822d6',
+        'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+    }
+ };
 
 const App = () => {
+
+    const [anime, setAnime] = useState([])
+    const searchTitle = async (title) => {
+        const response = await fetch(`${API_URL}&search=${title}`, options);
+        const result = await response.json()
+
+        setAnime(result.data)
+        console.log(result.data)
+    }
+
+
+    useEffect(() => {
+        searchTitle('akira')
+    },[])
+
     return (
         <div className="app">
             <h1>Aniview</h1>
@@ -22,9 +49,20 @@ const App = () => {
                 />
             </div>
 
-            <div className="container">
-
-            </div>
+            {
+                anime?.length > 0
+                    ? (
+                        <div className="container">
+                            {anime.map((anime) => (
+                                <AnimeCard anime={anime}/>
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No anime found</h2>
+                        </div>
+                    )
+            }
         </div>
     )
 }
